9137293bc0a71016b1b978c557599265d2eadb2c
Daan Rosbergen <d.rosbergen@gmail.com>
2018-10-17 22:16:28 +0200

    Fix websocket routing

--- packages/websocket/src/app.js
+++ packages/websocket/src/app.js
@@ -9,28 +9,28 @@ app.get('/healthz', (req, res, next) => {
     })
 })
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//     const err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
-// error handler
-// no stacktraces leaked to user unless in development environment
-app.use((err, req, res, next) => {
-    err.status = err.status || 500
+// // error handler
+// // no stacktraces leaked to user unless in development environment
+// app.use((err, req, res, next) => {
+//     err.status = err.status || 500
 
-    if (process.env.NODE_ENV !== 'test')
-        console.error('ERROR:', err)
+//     if (process.env.NODE_ENV !== 'test')
+//         console.error('ERROR:', err)
 
-    res.status(err.status);
-    res.json({
-        status: (err.status > 300 && err.status < 500) ? 'fail' : 'error',
-        message: err.message,
-        code: err.code || '',
-        stack: (process.env.NODE_ENV === 'development') ? err.stack : {},
-    });
-});
+//     res.status(err.status);
+//     res.json({
+//         status: (err.status > 300 && err.status < 500) ? 'fail' : 'error',
+//         message: err.message,
+//         code: err.code || '',
+//         stack: (process.env.NODE_ENV === 'development') ? err.stack : {},
+//     });
+// });
 
 module.exports = app
